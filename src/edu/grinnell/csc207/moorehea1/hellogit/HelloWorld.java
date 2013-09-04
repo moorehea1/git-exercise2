99ec8160205a9a1dfa97e3538531e95006214361
moorehea1 <moorehea1@grinnell.edu>
2013-09-04 10:42:55 -0500

    Update HelloWorld.java
    GitHub edits

--- src/edu/grinnell/csc207/moorehea1/hellogit/HelloWorld.java
+++ src/edu/grinnell/csc207/moorehea1/hellogit/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.grinnell.csc207.moorehea1.hellogit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello, World!");
     } // main(String[])
 }
